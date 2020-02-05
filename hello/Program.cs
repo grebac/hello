c7c4a0f03d03c11b687cb1586d9f5cfabef6f991
yikes <sami.caberg@hotmail.com>
2020-02-05 17:07:59 +0100

    :bug: modif du printf

--- hello/Program.cs
+++ hello/Program.cs
@@ -10,7 +10,7 @@ namespace hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world\n");
+            Console.WriteLine("hello git\n");
             Console.ReadKey();
         }
     }
