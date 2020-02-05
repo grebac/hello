1e976ad81d3c5e1b9ed19b227974b3dc40ad5f54
SayorisBodyguard <cyril.debattice@student.hepl.be>
2020-02-05 17:20:33 +0100

    Ajout de lyrics d'une chanson de REDNEX

--- hello/Program.cs
+++ hello/Program.cs
@@ -12,6 +12,12 @@ namespace hello
         {
             Console.WriteLine("hello git\n");
             Console.ReadKey();
+
+
+            /*If it hadn't been for Cotton-Eye Joe
+                I'd been married long time ago
+                Where did you come from, where did you go?
+                Where did you come from, Cotton-Eye Joe?*/
         }
     }
 }
